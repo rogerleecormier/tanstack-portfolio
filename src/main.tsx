eb49e5098b1679c25db8cecbadbcef1c323293eb
Roger Cormier <rogerleecormier@gmail.com>
2025-08-17 19:43:26 -0400

    fix: wrap AppRouter in HashRouter for proper routing functionality

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { HashRouter } from 'react-router-dom'
 import AppRouter from './router'
 import './index.css'
 
@@ -9,7 +10,9 @@ const queryClient = new QueryClient()
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <AppRouter />
+      <HashRouter>
+        <AppRouter />
+      </HashRouter>
     </QueryClientProvider>
   </React.StrictMode>,
 )
